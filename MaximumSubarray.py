a87f823d00ed7ffc87f28d498f0326caf39a9bc2
ilovedadata <106730909+ilovedadata@users.noreply.github.com>
2024-10-07 20:44:34 +0200

    Delete MaximumSubarray.py

--- MaximumSubarray.py
+++
@@ -1,8 +0,0 @@
-def max_subarray(numbers):
-    max_sum, cur_sum = float('-inf'), 0
-
-    for num in numbers:
-        cur_sum = max(cur_sum + num, num)
-        max_sum = max(max_sum, cur_sum)
-    
-    return max_sum
