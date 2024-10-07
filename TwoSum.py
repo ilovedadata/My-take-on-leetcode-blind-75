721b05259b4a658712ce6558a7886e10d0578fad
ilovedadata <106730909+ilovedadata@users.noreply.github.com>
2024-10-07 20:45:45 +0200

    Delete TwoSum.py

--- TwoSum.py
+++
@@ -1,8 +0,0 @@
-def twoSum(nums:list[int], target:int) -> list[int]:
-    hash_map = {}
-    for i, value in enumerate(nums):
-        delta = target - value
-        if delta in hash_map.keys():
-            return [hash_map[delta], i]
-        hash_map[value] = i
-    return
